089157f24cd32035c740d787a290703528f25717
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-31 14:51:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts "#{season.capitalize}:"
+    puts "#{season.to_s.capitalize}:"
+      season.each do |holiday, supplies|
+        puts "#{holiday.split.capitalize.join(" ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
