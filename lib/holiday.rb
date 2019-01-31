8943e0f967bc5bcdaeecdb1b95cc406ade0ace06
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-31 14:52:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
       season.each do |holiday, supplies|
-        puts "#{holiday.split.capitalize.join(" ")}: supplies"
+        puts "#{holiday.to_s.split.capitalize.join(" ")}: supplies"
       end
     end
 end
